3070163ea3a42bf1342b2fd3e190bf90a29425c2
Jin garam <2001garam@naver.com>
2021-12-03 23:11:39 +0900

    [Fix] 토큰 유효기간 수정 #134

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 // user._id를 통해 토큰 생성
 const generateAccessToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, { expiresIn: '20s' });
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
 };
 
 export default generateAccessToken;
