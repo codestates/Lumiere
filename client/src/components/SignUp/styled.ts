53a5135efdade2e87e6afa8ebaaff6fd70704392
Junghun Kim <aldzkwp1912@gmail.com>
2021-12-04 00:45:25 +0900

    [Feat] 회원가입 페이지 CSS, 반응형작업 Close #170

---
+++ client/src/components/SignUp/styled.ts
@@ -0,0 +1,161 @@
+import styled from 'styled-components/macro';
+
+// SigninTitle.tsx
+export const SignUpTitleWrap = styled.div`
+  text-align: center;
+  span {
+    font-size: 0.8rem;
+  }
+
+  @media only screen and (min-width: 768px) {
+    span {
+      font-size: 0.9rem;
+    }
+  }
+`;
+
+export const SignUpSub = styled.h1`
+  margin-bottom: 0.8rem;
+  font-size: 1.35rem;
+  font-weight: 800;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
+
+export const MoveSignInBtn = styled.button`
+  font-size: 1rem;
+  font-weight: 700;
+  border: none;
+  background: none;
+  text-decoration: underline;
+  text-decoration-color: #473f3f;
+  text-underline-position: under;
+
+  &:hover {
+    color: var(--color-red);
+    text-decoration-color: var(--color-red);
+    cursor: pointer;
+  }
+`;
+// SignInContent.tsx
+export const SignUpContentWrap = styled.div`
+  margin-top: 1rem;
+
+  @media only screen and (min-width: 768px) {
+    width: 100%;
+    margin-top: 2.5rem;
+  }
+`;
+
+export const SignUpInpuForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  /* height: 100%; */
+
+  & > label {
+    font-weight: 700;
+    font-size: 1rem;
+    position: relative;
+  }
+
+  & > label input {
+    display: block;
+    width: 100%;
+    height: 2.3rem;
+    margin-top: 0.7rem;
+    border-radius: 4px;
+    border: 1px solid #dcd9d9;
+    padding: 0 0 0 0.5rem;
+  }
+  input::placeholder {
+    font-size: 0.9rem;
+  }
+`;
+
+export const SignUpErrImg = styled.div`
+  position: absolute;
+  top: 2rem;
+  right: 0;
+  z-index: -1;
+
+  svg {
+    font-size: 1.6rem;
+    color: red;
+  }
+`;
+
+export const SignUpErrMessage = styled.span`
+  display: block;
+  margin: 0.4rem 0 0.8rem 0;
+  font-size: 0.6rem;
+  color: red;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 0.85rem;
+  }
+`;
+
+export const SignUpBtnWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+
+  button {
+    height: 4rem;
+    font-size: 1.35rem;
+    font-weight: 800;
+    color: #ffffff;
+    background-color: var(--color-black);
+    border-radius: 4px;
+    border: none;
+    transition: all 0.5s;
+
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.05);
+    }
+  }
+`;
+
+// SignUpPrivacy.tsx
+export const SignUpPrivacyWrap = styled.div`
+  margin: 0.1rem 0 1rem 0;
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    font-weight: 700;
+    height: 2rem;
+  }
+
+  input {
+    margin-right: 0.8rem;
+    vertical-align: middle;
+  }
+
+  button {
+    padding: 0 0 0 0.5rem;
+    color: green;
+    font-size: 0.9rem;
+    border: none;
+    background: none;
+  }
+
+  @media only screen and (min-width: 768px) {
+    margin: 2rem 0 3rem 0;
+
+    input[type='checkbox'] {
+      transform: scale(1.5);
+    }
+
+    input {
+      vertical-align: text-top;
+    }
+  }
+`;
--- client/src/pages/SignUpPage/SignUp.tsx
+++ client/src/pages/SignUpPage/SignUp.tsx
@@ -1,9 +1,19 @@
+import SignUpTitle from 'components/SignUp/SignUpTitle';
+import SignUpContent from 'components/SignUp/SignUpContent';
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
+import { SignUpContainer, SignUpWrap } from './styled';
+
 const SignUp = () => {
   return (
-    <>
-      <h1>SignUp</h1>
-      <span>hello</span>
-    </>
+    <SignUpContainer>
+      <Header />
+      <SignUpWrap>
+        <SignUpTitle />
+        <SignUpContent />
+      </SignUpWrap>
+      <Footer />
+    </SignUpContainer>
   );
 };
 export default SignUp;
