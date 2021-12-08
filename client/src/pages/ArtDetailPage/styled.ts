38cf541ee660ec0bc780a3a8cb563b9ffb22283c
devTiana <tmsh0510@gmail.com>
2021-12-09 03:24:05 +0900

    [Feat] ArtDetail 작품상세정보 서버데이터 랜더링 Close #43
    - 반응형 UI 구현
    - 응답받은 작품상세정보 서버데이터 랜더링

--- client/src/pages/ArtDetailPage/styled.ts
+++ client/src/pages/ArtDetailPage/styled.ts
@@ -2,4 +2,178 @@ import styled from 'styled-components';
 
 export const ArtDetailContainer = styled.div`
   margin-top: 6vh;
+  margin-bottom: 5rem;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 0;
+  }
+`;
+
+export const ArtDetailWrap = styled.div`
+  max-width: 1152px;
+  margin: 0 auto;
+  padding: 0 1rem;
+`;
+
+export const DetailInfoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div.test {
+    height: 100vh;
+    background: lavender;
+  }
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    padding: 2rem 1rem;
+
+    div.test {
+      width: 66%;
+      height: 100vh;
+      background: lavender;
+    }
+  }
+`;
+
+export const DetailImgBox = styled.div`
+  max-height: 40vh;
+  height: 40vh;
+  padding: 1.5rem 1rem 2rem 0.5rem;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    filter: drop-shadow(0.5rem 0.5rem 0px rgba(0, 0, 0, 0.25));
+  }
+
+  @media screen and (min-width: 768px) {
+    /* flex: 1; */
+    width: 66%;
+    padding: 2rem 2rem 2.5rem 1.5rem;
+    background-color: #f4f4f4;
+  }
+`;
+
+export const DetailInfoBox = styled.div`
+  margin: 0.5rem 0 1rem;
+  font-weight: 700;
+
+  h4 {
+    margin: auto 0;
+    font-size: 1.2rem;
+  }
+  > div {
+    display: flex;
+    padding: 0.4rem 0;
+    line-height: 1.2;
+
+    > span {
+      width: 5rem;
+      color: #aaaaaa;
+    }
+    > div {
+      flex: 1;
+    }
+  }
+  > div:first-child {
+    margin-bottom: 0.5rem;
+    padding: 0.4rem 0;
+    > div {
+      display: none;
+    }
+  }
+  > div:nth-child(2) div a {
+    display: flex;
+    align-items: center;
+  }
+  > div:nth-child(5) {
+    > span {
+      margin: auto 0;
+    }
+    > div {
+      font-size: 1.2rem;
+      font-weight: 800;
+      text-align: right;
+    }
+  }
+  > div:last-child {
+    padding: 0;
+  }
+
+  @media screen and (min-width: 768px) {
+    position: sticky;
+    top: 7vh;
+    width: 30%;
+    margin: 0;
+
+    > div:first-child div {
+      display: block;
+      text-align: right;
+      svg {
+        width: 2rem;
+        height: 100%;
+        margin-left: 0.4rem;
+        padding: 0.4rem;
+        color: #aaaaaa;
+      }
+    }
+  }
+`;
+
+export const OrderBtnBox = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  width: 100%;
+
+  > div {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 3.5rem;
+    margin: 0;
+    padding: 0;
+    font-size: 1.2rem;
+    font-weight: 800;
+    background-color: #eeeeee;
+    &.primary_button {
+      color: #ffffff;
+      background-color: var(--color-black);
+    }
+  }
+  > a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 3.5rem;
+    height: 3.5rem;
+    font-size: 1.5rem;
+    color: #aaaaaa;
+    background-color: #ffffff;
+    border: 1px solid #eeeeee;
+  }
+
+  @media screen and (min-width: 768px) {
+    position: relative;
+    margin-top: 0.5rem;
+
+    > div {
+      height: 2.8rem;
+      font-size: 1rem;
+      transition: all 0.5s;
+      &:hover {
+        font-size: 1.2rem;
+      }
+    }
+    > a {
+      display: none;
+    }
+  }
 `;
