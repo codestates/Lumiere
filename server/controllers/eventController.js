5da3866d8e05e69264f2b33ae897f136a1630856
Jin garam <2001garam@naver.com>
2021-12-10 01:21:05 +0900

    [Fix] 배너 불필요 핸들러 제거
    - getEventById 핸들러 제거 (모달이라 요청 불필요)
    - addEvent 필요 데이터 유효성 확인

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -5,8 +5,13 @@ import Event from '../models/event.js';
 // @route  POST /api/events
 // @access Private/Admin
 const addEvent = asyncHandler(async (req, res) => {
-  const event = await Event.create(req.body);
-  res.status(201).json(event);
+  const { heading, content, linkname, link, image } = req.body;
+  if (heading && content && linkname && link && image) {
+    const event = await Event.create(req.body);
+    res.status(201).json(event);
+  } else {
+    res.status(400).json({ message: '내용을 모두 입력해주세요' });
+  }
 });
 
 // @desc   Fetch all events
@@ -17,20 +22,8 @@ const getEvents = asyncHandler(async (req, res) => {
   res.json(events);
 });
 
-// @desc   Fetch single event
-// @route  GET /api/events/:id
-// @access Private/Admin
-const getEventById = asyncHandler(async (req, res) => {
-  const event = await Event.findById(req.params.id);
-
-  if (event) res.json(event);
-  else {
-    res.status(404).json({ message: '해당 배너가 존재하지 않습니다' });
-  }
-});
-
 // @desc   Update a event
-// @route  PATCH /api/events/:id
+// @route  PATCH /api/events/
 // @access Private/Admin
 const updateEvent = asyncHandler(async (req, res) => {
   const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
@@ -40,11 +33,11 @@ const updateEvent = asyncHandler(async (req, res) => {
 });
 
 // @desc   Delete the event
-// @route  DELETE /api/events/:id
+// @route  DELETE /api/events/
 // @access Private/Admin
 const deleteEvent = asyncHandler(async (req, res) => {
-  await Event.findByIdAndDelete(req.params.id);
+  await Event.deleteOne(req.params.id);
   res.status(200).json({ messsage: '해당 배너가 정상적으로 삭제되었습니다' });
 });
 
-export { getEvents, getEventById, addEvent, updateEvent, deleteEvent };
+export { getEvents, addEvent, updateEvent, deleteEvent };
