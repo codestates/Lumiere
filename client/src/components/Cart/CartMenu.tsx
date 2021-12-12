87f2fdf941d5acc9d2ffcd56ef76524643d00754
Junghun Kim <aldzkwp1912@gmail.com>
2021-12-12 11:45:25 +0900

    [Feat] Cart 품절 작품 삭제 기능 구현 Close #53
    - [GET] 로컬스토리지 'cartItems' 작품정보 목록 중 고유아이디만 뽑아서 서버에 재고 확인 요청
    성공 : 품절여부 목록 받아서 로컬스토리지 작품 목록 상태(inStock) 업데이트 후 false인 작품만 필터링하여 맵으로 렌더링

--- client/src/components/Cart/CartMenu.tsx
+++ client/src/components/Cart/CartMenu.tsx
@@ -1,7 +1,34 @@
+/* eslint no-underscore-dangle: 0 */
+import instance from 'util/axios';
+import { OrderProducts } from 'util/type';
 import { VscPass } from 'react-icons/vsc';
 import { CartMenuWrap, AllSelectBtnWrap, SelectBtnWrap } from './styled';
 
 export const CartMenu = () => {
+  const soldoutDeleteHandler = () => {
+    const cartItems = localStorage.getItem('cartItems');
+    const arr = JSON.parse(cartItems || '{}');
+    instance
+      .get('/products/cart-items', { params: { productId: arr } })
+      .then((res) => {
+        const newArr = res.data.filter((el: OrderProducts) => {
+          return el.inStock === true;
+        });
+        localStorage.setItem(
+          'cartItems',
+          JSON.stringify(
+            newArr.map((el: OrderProducts) => {
+              return el._id;
+            }),
+          ),
+        );
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <CartMenuWrap>
       <AllSelectBtnWrap>
@@ -13,7 +40,9 @@ export const CartMenu = () => {
         </button>
       </AllSelectBtnWrap>
       <SelectBtnWrap>
-        <button type="button">품절상품삭제</button>
+        <button type="button" onClick={soldoutDeleteHandler}>
+          품절상품삭제
+        </button>
         <button type="button">선택상품삭제</button>
       </SelectBtnWrap>
     </CartMenuWrap>
