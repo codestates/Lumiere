cca25f0c01430db6fa09feed9566535f87f94f22
Junghun Kim <aldzkwp1912@gmail.com>
2021-12-16 03:58:47 +0900

    [Feat] Mypage 최초 랜더링 Close #67
    - 토큰으로 유저 주문내역 서버 요청 (로딩)
    성공 : 응답받은 데이터 주문내역 컴포넌트에 전달 & 상태별로 카운트해서 적기(클라)
    실패 : 404 에러페이지로 이동
    주문내역 컴포넌트 랜더링(서버에서 전달 받은 값을 뿌리기)

--- client/src/components/OrderHistory/MyPageOrderMenu.tsx
+++ client/src/components/OrderHistory/MyPageOrderMenu.tsx
@@ -5,7 +5,7 @@ const MypageOrderMenu = () => {
     <MenuContainer>
       <MenuListWrapUl>
         <li>주문 번호</li>
-        <li>상품 정보</li>
+        <li>작품 정보</li>
         <li>총 결제 금액</li>
         <li>진행 상태</li>
         <li>관리</li>
