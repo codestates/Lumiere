45c8f951e09dca033dae0a40cb3ecdb08fbecd8a
Jinhyun Park <developerjhp@gmail.com>
2021-12-19 19:03:14 +0900

    [Fix]구글 로그인 오류 수정 close #568

--- client/src/pages/SignInPage/SignIn.tsx
+++ client/src/pages/SignInPage/SignIn.tsx
@@ -2,9 +2,15 @@ import SignInTitle from 'components/SignIn/SignInTitle';
 import SignInContent from 'components/SignIn/SignInContent';
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
+import { useEffect } from 'react';
 import { SignInWrap, SignInContainer } from './styled';
 
 const SignIn = () => {
+  useEffect(() => {
+    const local = localStorage.getItem('recoil-persist');
+    const res = JSON.parse(local || '{}').LoginState;
+    if (res) window.location.reload();
+  }, []);
   return (
     <SignInContainer>
       <Header />
